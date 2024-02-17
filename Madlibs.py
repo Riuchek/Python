7dc629eedd52ca3c752893285ab80f1675e43bb8
João Marinho <87945566+Riuchek@users.noreply.github.com>
2024-02-16 21:53:03 -0300

    Delete Madlibs.py

--- Madlibs.py
+++
@@ -1,6 +0,0 @@
-youtuber = "tft toddy"
-print(f"melhor playe {youtuber}")
-adj = input("adjetivo para o tft toddy: ")
-babu = input("fala outo ")
-madlib = f"ele fez 3 dragoes no 2-1 {adj} o cara eh {babu}"
-print(madlib)
